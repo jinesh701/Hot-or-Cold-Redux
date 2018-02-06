00d79aa4c3336259e95cbfaa83488ac0dbe64317
Jinesh Patel <jinesh701@gmail.com>
2018-02-05 23:36:39 -0500

    corrected reducer to show numbers instead of NaN

--- src/reducer.js
+++ src/reducer.js
@@ -20,7 +20,7 @@ export default (state = initialState, action) => {
   if (action.type === MAKE_GUESS) {
     let feedback, guess;
 
-    guess = parseInt(guess, 10);
+    guess = parseInt(action.guess, 10);
     if (isNaN(guess)) {
       feedback = "Please enter a valid number";
 
@@ -30,7 +30,7 @@ export default (state = initialState, action) => {
       });
     }
 
-    const difference = Math.abs(guess - this.state.correctAnswer);
+    const difference = Math.abs(guess - state.correctAnswer);
 
     if (difference >= 50) {
       feedback = "You're Ice Cold...";
